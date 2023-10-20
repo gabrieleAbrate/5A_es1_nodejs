4f18309b256533c6ee9a9700b2595913483e5e2a
Gabriele Abrate <gabriele.abrate.2005@gmail.com>
2023-10-20 09:09:38 +0200

    correzzione dello switch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // http -> modulo indispensabile se voglio creare una comunicazione client-server
 // url -> analizzare e parsificare l'url 
 // require -> come una comune import
+const { info } = require('console');
 const http = require('http');
 const url = require('url');
 
@@ -24,6 +25,31 @@ let server = http.createServer((richiesta, risposta) => {
         parametri: ${infoUrl.search}
     `;
 
+    let header;
+    switch(infoUrl.pathname){
+        case '/richiesta':
+            stampaRichiesta(testoRisposta, risposta, infoUrl);
+            break;
+        case '/index':
+            header = {'Content-Type': 'text/html'};
+            risposta.writeHead(200, header);
+            risposta.write('<h1>Benvenuto nella home page</h1>');
+            risposta.end();
+            break;
+        default:
+            header = {'Content-Type': 'text/plain'};
+            risposta.writeHead(404, header);
+            risposta.write('nessuna risorsa trovata');
+            risposta.end();
+            break;
+    }
+});
+
+// fondamentale per avviare il server
+server.listen(1337);
+console.log('il server è avviato sulla porta 1337');
+
+function stampaRichiesta(testoRisposta, risposta, infoUrl){
     // recupero i parametri dall'url separati dal ?
     let parametri = infoUrl.query;
     testoRisposta += `
@@ -32,7 +58,8 @@ let server = http.createServer((richiesta, risposta) => {
     `;
 
     // 1° parametro: codice di risposta -> 200 = OK
-    // 2° parametro: oggetto json -> insieme di opzioni che vogliamo inserire nell'intestazione
+    // 2° parametro: oggetto json -> insieme di opzioni che vogliamo inserire nell'intestazione 
+    // text/plain, text/html, application/json
     let header = {'Content-Type': 'text/plain'};
     risposta.writeHead(200, header);
 
@@ -41,8 +68,4 @@ let server = http.createServer((richiesta, risposta) => {
 
     // informo il server che ho finito di preparare il pacchetto di risposta
     risposta.end();
-});
-
-// fondamentale per avviare il server
-server.listen(1337);
-console.log('il server è avviato sulla porta 1337');
+}
